489d2db28841a02b5378b31ec493301135a7cd48
Miracle-229 <zzzzhd02@gmail.com>
2023-09-01 22:11:37 +0300

    add button for user in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,26 +1,30 @@
 import { IGameData } from '@/types/home';
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useId, useState } from 'react';
 import styles from '../../styles/Style.module.css';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { AsyncTypeahead } from 'react-bootstrap-typeahead';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { BiSolidUser, BiSolidBookmarks, BiSolidDoorOpen } from 'react-icons/bi';
 import { handleSearch } from '@/api/api';
+import { useSession, signOut } from 'next-auth/react';
 
 type Props = {};
 
 const Header = (props: Props) => {
   const [isLoading, setIsLoading] = useState(false);
   const [options, setOptions] = useState<any[]>([]);
+  const user = useSession();
+  const id = useId();
 
- async function wrapperHandleSearch(query:string){
-    await handleSearch(query,setIsLoading,setOptions)
+  async function wrapperHandleSearch(query: string) {
+    await handleSearch(query, setIsLoading, setOptions);
   }
 
   const router = useRouter();
   const handleGameSelect = (selected: any) => {
-    console.log(selected)
+    console.log(selected);
     if (selected.length > 0) {
       const slug = selected[0].slug;
       router.push(`/games/${slug}`);
@@ -34,6 +38,7 @@ const Header = (props: Props) => {
       </Link>
       <Nav className="me-auto">
         <AsyncTypeahead
+          key={id}
           id="search-bar"
           isLoading={isLoading}
           labelKey="label"
@@ -94,6 +99,39 @@ const Header = (props: Props) => {
         >
           Games
         </Link>
+        {user?.data ? (
+          <NavDropdown
+            title={<BiSolidUser size={30} />}
+            id="navbarScrollingDropdown"
+          >
+            <NavDropdown.Item>
+              <Link
+                style={{ color: 'gray' }}
+                onClick={() => window.scrollTo(0, 0)}
+                href={`/account_movies`}
+              >
+                <BiSolidBookmarks style={{ marginBottom: '3px' }} /> Collection
+              </Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action4">
+              <Link
+                href="#"
+                style={{ color: 'gray' }}
+                onClick={() => signOut({ callbackUrl: '/' })}
+              >
+                <BiSolidDoorOpen style={{ marginBottom: '3px' }} /> Exit
+              </Link>
+            </NavDropdown.Item>
+          </NavDropdown>
+        ) : (
+          <Link
+            style={{ color: 'gray' }}
+            onClick={() => window.scrollTo(0, 0)}
+            href={`/api/auth/signin`}
+          >
+            <BiSolidDoorOpen size={30} />
+          </Link>
+        )}
       </Navbar.Collapse>
     </Navbar>
   );
